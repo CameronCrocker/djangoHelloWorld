60cd6f9ba76dadbacad17124eaf6385f7a047cbc
CameronCrocker <34074117+CameronCrocker@users.noreply.github.com>
2021-02-20 14:12:30 +0000

    added a simple clicker

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='django_simple_clicker',
+    version='',
+    packages=['djangoHelloWorld'],
+    url='',
+    license='',
+    author='Cameron',
+    author_email='',
+    description='Simple django clicker'
+)
